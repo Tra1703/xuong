038423c701506b6ca876a7beab94daff62d01712
Tra1703 <tradvph41451@fpt.edu.vn>
2025-04-12 23:08:41 +0700

    Add files via upload

---
+++ page.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trang Chủ Bán Hàng</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <nav>
+    <a href="#">Trang Chủ</a>
+    <a href="#">Cửa Hàng </a>
+    <a href="#">Sản Phẩm</a>
+    <a href="#">Giới Thiệu</a>
+    <a href="#">Liên Hệ</a>
+  </nav>
+  <h1>OUR ORANGIC PRODUCTS</h1>
+  <section class="products">
+    <div class="product">
+      <img src="image/1.jpg" alt="Sản phẩm 1">
+      <h3>Sản phẩm 1</h3>
+      <p>Giá: 500.000₫/kg </p>
+    </div>
+    <div class="product">
+      <img src="image/2.jpg" alt="Sản phẩm 2">
+      <h3>Sản phẩm 2</h3>
+      <p>Giá: 600.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/3.jpg" alt="Sản phẩm 3">
+      <h3>Sản phẩm 3</h3>
+      <p>Giá: 700.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/4.jpg" alt="Sản phẩm 4">
+      <h3>Sản phẩm 4</h3>
+      <p>Giá: 800.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/5.jpg" alt="Sản phẩm 5">
+      <h3>Sản phẩm 5</h3>
+      <p>Giá: 900.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/6.jpg" alt="Sản phẩm 6">
+      <h3>Sản phẩm 6</h3>
+      <p>Giá: 1.000.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/7.jpg" alt="Sản phẩm 7">
+      <h3>Sản phẩm 7</h3>
+      <p>Giá: 1.100.000₫/kg</p>
+    </div>
+    <div class="product">
+      <img src="image/8.jpg" alt="Sản phẩm 8">
+      <h3>Sản phẩm 8</h3>
+      <p>Giá: 1.200.000₫/kg</p>
+    </div>
+  </section>
+</body>
+</html>
